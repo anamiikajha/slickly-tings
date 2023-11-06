2fd8130eca16c3789a76898f73a7616ae5305dd5
Anamika Jha <89740849+anamiikajha@users.noreply.github.com>
2023-11-06 23:58:10 +0530

    Added Slider fade effect for  Carousel

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -3,30 +3,31 @@ import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import '/node_modules/swiper/swiper-bundle.min.css';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-
+import { EffectFade } from 'swiper/modules';
 
 function Slider() {
-  return (
+  return (                                                                                                          
     <Swiper
-    modules={[Navigation, Pagination, Scrollbar, A11y]}
+    modules={[Navigation, Pagination, Scrollbar, A11y, EffectFade]}
     spaceBetween={50}
-    slidesPerView={2}
+    effect="fade"
+    slidesPerView={1}
     navigation
     pagination={{ clickable: true }}
     scrollbar={{ draggable: true }}
     
   >
     <SwiperSlide>
-        <img src="public/kali.jpg" alt="" style={{width: "100%"}} />
+    <img src="public/kali.jpg" alt="" style={{width: "100%", height: "100vh"}}/>
     </SwiperSlide>
     <SwiperSlide>
-    <img src="public/background.jpg" alt=""  style={{width: "100%"}} />
+    <img src="public/background.jpg" alt="" style={{width:"100%", height: "100vh"}} />
         </SwiperSlide>
     <SwiperSlide>
-    <img src="public/bg.png" alt=""  style={{width: "100%"}} />
+    <img src="public/bg.png" alt=""  style={{width: "100%", height: "100vh"}} />
     </SwiperSlide>
     <SwiperSlide>
-    <img src="public/g.jpg" alt=""  style={{width: "100%"}} />
+    <img src="public/g.jpg" alt=""  style={{width: "100%", height: "100vh"}} />
     </SwiperSlide>
   </Swiper>
   )
